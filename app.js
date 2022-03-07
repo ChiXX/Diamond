b1b3726ad4df6692913ad4e7d54c511dd5833c86
chixu <thisischixu@gmail.com>
2022-03-07 01:17:39 +0100

    add comments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('.grid')
 const reset = document.querySelector('#reset')
 
 class diamond {
+	// diamond constructor
 	constructor(xAxis, yAxis, color) {
 	    this.diamond = document.createElement('div')
 		this.diamond.classList.add('diamond')
@@ -21,15 +22,18 @@ const diamonds = [
 	new diamond(10, 110, 'red'),
 	new diamond(10, 160, 'yellow'),
 ]
-
+//add click listener
 for (let i = 0; i < diamonds.length; i++){
 	diamonds[i].diamond.addEventListener('click', moveDiamond)
 }
 function moveDiamond(){
+	// get current position and color
 	const position = Number(this['style'].left.slice(0, -2))
 	const color = this['style']['background-color']
+	// move
 	this['style'].left = position + 50 + 'px'
 	displayScores(position, color)
+	// win
 	if (position === 460){
 		for (let i = 0; i < diamonds.length; i++){
 			diamonds[i].diamond.removeEventListener('click', moveDiamond)
@@ -38,13 +42,14 @@ function moveDiamond(){
 }
 
 function displayScores(position, color){
+	// identify the diamond with color
 	const score = document.querySelector('#' + color + '-score')		
 	score.textContent = Math.floor((position + 40) / 50)
 	if (Math.floor((position + 40) / 50) === 10){
 		score.style.color = 'red'
 	}
 }
-
+// reset
 reset.addEventListener('click', () => {
 	const scores = document.querySelectorAll('.score')
 	for (let i = 0; i < scores.length; i++){
