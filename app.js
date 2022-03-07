ef563a03da42c9eaf053a4f13f6894205ac1326c
chixu <thisischixu@gmail.com>
2022-03-07 01:12:15 +0100

    update

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const grid = document.querySelector('.grid')
 const reset = document.querySelector('#reset')
-const scores = Array.from(document.querySelectorAll('.score')).reverse()
-
-// reset function
-reset.addEventListener('click', () => {
-	const allDiamonds = Array.from(document.querySelectorAll('.diamond'))
-	// wipe the diamonds
-	for (let i = 0; i < allDiamonds.length; i++){
-		allDiamonds[i].classList.remove('diamond')
-	}
-	// reset the coordinates of diamonds
-	for (let i = 0; i < diamonds.length; i++ ){
-		diamonds[i].left = 10
-		diamonds[i].bottom = i * 50 + 10
-	}
-	// reset scores
-	for (let i = 0; i < scores.length; i++ ){
-		scores[i].textContent = 0
-		scores[i].style.color = 'black'
-	}
-	// redraw the diomands
-	drawDiamonds()
-})
 
 class diamond {
 	constructor(xAxis, yAxis, color) {
-	    this.left = xAxis
-		this.bottom = yAxis
-		this.color = color
+	    this.diamond = document.createElement('div')
+		this.diamond.classList.add('diamond')
+		this.diamond.style.left = xAxis + 'px'
+		this.diamond.style.bottom = yAxis + 'px'
+		this.diamond.style['background-color'] = color
+		grid.appendChild(this.diamond)
+	}
+	resetX(x){
+		this.diamond.style.left = x + 'px'
 	}
 }
 
@@ -38,47 +22,35 @@ const diamonds = [
 	new diamond(10, 160, 'yellow'),
 ]
 
-function drawDiamonds(){
-	for (let i = 0; i < diamonds.length; i++){
-		const diamond = document.createElement('div')
-		diamond.classList.add('diamond')
-		diamond.style.left = diamonds[i].left + 'px'
-		diamond.style.bottom = diamonds[i].bottom + 'px'
-		diamond.style['background-color'] = diamonds[i].color
-		diamond.setAttribute('diamond-id', i)
-		grid.appendChild(diamond)
-		diamond.addEventListener('click', moveDiamond)
-	}
+for (let i = 0; i < diamonds.length; i++){
+	diamonds[i].diamond.addEventListener('click', moveDiamond)
 }
-
-
 function moveDiamond(){
-	const diamondId = this.getAttribute('diamond-id')
-	const allDiamonds = Array.from(document.querySelectorAll('.diamond'))
-	// wipe current diamonds
-	for (let i = 0; i < allDiamonds.length; i++){
-		allDiamonds[i].classList.remove('diamond')
-	}
-	// update coodinates (move)
-	diamonds[diamondId].left += 50
-	drawDiamonds()
-	displayScores()
-	// check win
-	if (diamonds[diamondId].left === 510){
-		const allDiamonds = Array.from(document.querySelectorAll('.diamond'))
-		for (let i = 0; i < allDiamonds.length; i++){
-			allDiamonds[i].removeEventListener('click', moveDiamond)
+	const position = Number(this['style'].left.slice(0, -2))
+	const color = this['style']['background-color']
+	this['style'].left = position + 50 + 'px'
+	displayScores(position, color)
+	if (position === 460){
+		for (let i = 0; i < diamonds.length; i++){
+			diamonds[i].diamond.removeEventListener('click', moveDiamond)
 		}
 	}	
 }
 
-function displayScores(){
-	for(let i = 0; i < scores.length; i++){
-		scores[i].textContent = Math.floor((diamonds[i].left - 10) / 50)
-		if (Math.floor((diamonds[i].left - 10) / 50) === 10){
-			scores[i].style.color = 'red'
-		}
+function displayScores(position, color){
+	const score = document.querySelector('#' + color + '-score')		
+	score.textContent = Math.floor((position + 40) / 50)
+	if (Math.floor((position + 40) / 50) === 10){
+		score.style.color = 'red'
 	}
 }
 
-drawDiamonds()
+reset.addEventListener('click', () => {
+	const scores = document.querySelectorAll('.score')
+	for (let i = 0; i < scores.length; i++){
+		scores[i].textContent = 0
+		scores[i].style.color = 'black'
+		diamonds[i].resetX(10)
+		diamonds[i].diamond.addEventListener('click', moveDiamond)
+	}
+})
